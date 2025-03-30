ed9277117976e03298422b8bf2e875176e840bf2
Your Name <you@example.com>
2025-03-30 13:35:07 -0500

    Added comment to explain withComponentInputBinding in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
+    // adding withComponentInputBinding() to enable use of input signal
+    // to directly access route parameters.
     provideRouter(routes, withComponentInputBinding())
   ]
 };
